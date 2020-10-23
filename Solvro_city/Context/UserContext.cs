44701e55d63e703e757767b24ba62395c4f96e36
Adrian Żak <adrian.damian.zak@gmail.com>
2020-10-23 14:41:17 +0200

    Added .dockerignore. Removed appsettings from github, example provided instead.

--- Solvro_city/Context/UserContext.cs
+++ Solvro_city/Context/UserContext.cs
@@ -4,14 +4,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Solvro_city.Models;
-
+using Microsoft.Extensions.Configuration;
 
 namespace Solvro_city.Context
 {
+    /// <summary>
+    /// User context of database
+    /// </summary>
     public class UserContext : DbContext
     {
+        /// <summary>
+        /// Server configuration
+        /// </summary>
+        IConfiguration conf;
+
+        /// <summary>
+        /// User context constructor
+        /// </summary>
+        /// <param name="conf">Server confiuguration</param>
+        public UserContext(IConfiguration conf)
+        {
+            this.conf = conf;
+        }
+
+        /// <summary>
+        /// User table
+        /// </summary>
         public DbSet<User> Users { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder options) { options.UseSqlite("Data Source=users.db"); }
+        /// <summary>
+        /// Configure Sqlite
+        /// </summary>
+        /// <param name="options"></param>
+        protected override void OnConfiguring(DbContextOptionsBuilder options) { options.UseSqlite($"Data Source=users.db;Password={conf["Db:Password"]};"); }
     }
 }
